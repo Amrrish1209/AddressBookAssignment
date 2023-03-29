8aae4d6694561a76fdb02ed1f73b6b6d701ad8f9
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-29 16:23:19 +0530

    Add: Start with Displaying Welcome to Address Book Program in AddressBookMain class

---
+++ src/com/bridgelabz/addressbook/AddressBook.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.addressbook;
+
+import java.util.Scanner;
+
+public class AddressBook extends Contact{
+	
+	public void addContact() {
+		
+		Scanner sc = new Scanner()
+	}
+}
