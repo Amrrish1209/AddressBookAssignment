3c71780a57377a57cb6363855a577e9494eae2e0
Amrrish1209 <roshanbabubl@gmail.com>
2023-03-29 16:12:37 +0530

    Add: Ability to create a Contacts in Address Book with first and last names, address, city, state, zip, phone number and email...

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -5,31 +5,6 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome To Address Book Program");
-		System.out.println("");
-		System.out.println("New Contact created");
-		Contact contact = new Contact();
-		contact.setFirstName("Amrrish");
-		System.out.println("FirstName: "+contact.getFirstName());
 		
-		contact.setLastName("Roshan");
-		System.out.println("LastName: "+contact.getLastName());
-		
-		contact.setAddress("Kottar");
-		System.out.println("Address: "+contact.getAddress());
-		
-		contact.setCity("Nagercoil");
-		System.out.println("CityName: "+contact.getCity());
-		
-		contact.setState("TamilNadu");
-		System.out.println("StateName: "+contact.getState());
-		
-		contact.setZip(629002);
-		System.out.println("Zip: "+contact.getZip());
-		
-		contact.setPhoneNumber("8778213740");
-		System.out.println("PhoneNumber: "+contact.getPhoneNumber());
-		
-		contact.setEmail("roshanbabubl@gmail.com");
-		System.out.println("EmailId: "+contact.getEmail());
 	}
 }
